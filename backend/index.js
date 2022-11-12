e91cd95db2974210c2f37ab4d9555a7df22666fb
Trang-K65ccuet <20020482@vnu.edu.vn>
2022-11-12 22:22:11 +0700

    Create basic

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,28 @@
-console.log("hello");
+import express from "express";
+import cors from "cors";
+import session from "express-session"
+import dotenv from "dotenv"
+import db from "./config/Database.js"
+dotenv.config();
+
+const app = express();
+
+app.use(session({
+    secret: process.env.SESS_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: 'auto'
+    }
+}))
+
+app.use(cors({
+    credentials: true,
+    origin : 'http://localhost:3000'
+}));
+
+app.use(express.json());
+
+app.listen(process.env.APP_PORT, ()=> {
+    console.log("Server up and running...")
+});
