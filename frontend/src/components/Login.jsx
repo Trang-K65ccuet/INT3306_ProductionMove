8c941c5c587ef37c4e46a81e5493a3e9a5e21fb9
Trang-K65ccuet <20020482@vnu.edu.vn>
2022-11-26 18:16:38 +0700

    login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,22 +1,46 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { LoginUser, reset } from "../features/authSlice";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { user, isError, isSuccess, isLoading, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (user || isSuccess) {
+      navigate("/dashboard");
+    }
+    dispatch(reset());
+  }, [user, isSuccess, dispatch, navigate]);
+
+  const Auth = (e) => {
+    e.preventDefault();
+    dispatch(LoginUser({ username, password }));
+  };
+
   return (
     <section className="hero is-fullheight is-fullwidth">
       <div className="hero-body">
         <div className="container">
           <div className="columns is-centered">
             <div className="column is-4">
-              <form className="box"> 
+              <form onSubmit={Auth} className="box">
                 <h1 className="title is-2">Sign In</h1>
                 <div className="field">
-                  <label className="label">Email</label>
+                  <label className="label">Username</label>
                   <div className="control">
                     <input
                       type="text"
                       className="input"
-                      placeholder="Email"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                      placeholder="Username"
                     />
                   </div>
                 </div>
@@ -26,12 +50,20 @@ const Login = () => {
                     <input
                       type="password"
                       className="input"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
                       placeholder="******"
                     />
                   </div>
                 </div>
                 <div className="field mt-5">
-                  <NavLink to={"/dashboard"} className="button is-success is-fullwidth">Login</NavLink>
+                  <button
+                    type="submit"
+                    className="button is-success is-fullwidth"
+                  >
+                    {isLoading ? "Loading..." : "Login"}
+                  </button>
+                  {isError && <p className="has-text-centered">{message}</p>}
                 </div>
               </form>
             </div>
