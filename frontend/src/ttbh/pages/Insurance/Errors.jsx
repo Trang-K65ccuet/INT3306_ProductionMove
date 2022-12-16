a601d1c0876790f500f1910a8377a3e21eed2e03
Trang-K65ccuet <20020482@vnu.edu.vn>
2022-12-16 18:21:08 +0700

    frontend

--- frontend/src/ttbh/pages/Insurance/Errors.jsx
+++ frontend/src/ttbh/pages/Insurance/Errors.jsx
@@ -4,7 +4,33 @@ const ErrorsTTBH = () => {
 
   return (
     <Layout>
-      Thất bại
+        <div>
+          <h1 className="title">Không thể bảo hành</h1>
+            <table className="table is-striped is-fullwidth">
+              <thead>
+                <tr>
+                  <th>STT</th>
+                  <th>Mặt hàng</th>
+                  <th>Số lượng bảo hành</th>
+                  <th>Đại lý phân phối</th>
+                  <th>Cơ sở sản xuất</th>
+                  <th>Hành động</th>
+                </tr>
+              </thead>
+              <tbody>
+              <tr>
+                    <td>1</td>
+                    <td>Asus</td>
+                    <td>1000</td>
+                    <td>Đại lý 1</td>
+                    <td>Cơ sở của Trang</td>
+                    <td>
+                      <button className="button is-small is-danger">Chuyển về cơ sở sản xuất</button>
+                    </td>
+                  </tr>
+              </tbody>
+            </table>
+        </div>
     </Layout>
   );
 };
--- frontend/src/ttbh/pages/Insurance/Successful.jsx
+++ frontend/src/ttbh/pages/Insurance/Successful.jsx
@@ -4,7 +4,31 @@ const SuccessfulTTBH = () => {
 
   return (
     <Layout>
-      Thành công
+        <div>
+          <h1 className="title">Đã bảo hành</h1>
+            <table className="table is-striped is-fullwidth">
+              <thead>
+                <tr>
+                  <th>STT</th>
+                  <th>Mặt hàng</th>
+                  <th>Số lượng bảo hành</th>
+                  <th>Đại lý phân phối</th>
+                  <th>Hành động</th>
+                </tr>
+              </thead>
+              <tbody>
+              <tr>
+                    <td>1</td>
+                    <td>Asus</td>
+                    <td>1000</td>
+                    <td>Đại lý 1</td>
+                    <td>
+                      <button className="button is-small is-info">Chuyển về đại lý</button>
+                    </td>
+                  </tr>
+              </tbody>
+            </table>
+        </div>
     </Layout>
   );
 };
