db7b189da2bc7c8dfdfdcf5974f188882b56f347
Trang-K65ccuet <20020482@vnu.edu.vn>
2022-12-21 21:25:05 +0700

    dashboard ttbh

--- frontend/src/ttbh/pages/Dashboard.jsx
+++ frontend/src/ttbh/pages/Dashboard.jsx
@@ -1,12 +1,21 @@
 import Layout from "./Layout";
-import Welcome from "../components/Welcome";
+import React, { useEffect } from "react";
+import Widget from "../widget/Widget";
+import Chart from "../chart/Chart";
+import "./pages.css"
 
 
 const DashboardTTBH = () => {
-
   return (
     <Layout>
-      <Welcome />
+      <div className="widgets">
+            <Widget type="products" />
+            <Widget type="insurance" />
+            <Widget type="error" />
+        </div>
+        <div className="charts">
+          <Chart title="Trong năm nay" aspect={3 / 1} />
+        </div>
     </Layout>
   );
 };
