3225230542c34fe3a80217902c727fa1da0bf2b5
phanducmanhk65 <phanducmanh2812002@gmail.com>
2022-12-23 17:51:29 +0700

    manh up

--- backend/routes/ManufactureRoute.js
+++ backend/routes/ManufactureRoute.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { getAllRequestByManufacture, getManufactures , getProductitemByManufacture} from "../controllers/manufacture/ManufactureController.js";
-import { authorizationUser } from "../middleware/AuthUser.js";
+import { getAllRequestByManufacture, getManufactures , getProductitemByManufacture, sendListProductItem } from "../controllers/manufacture/ManufactureController.js";
+import { authorizationUser, checkCssx } from "../middleware/AuthUser.js";
 // route cho cơ sở sản xuất
 const manufactureRouter = express.Router();
 
 manufactureRouter.get('/manufactures/all', authorizationUser, getManufactures);
 manufactureRouter.get('/manufactures/items',authorizationUser, getProductitemByManufacture);
 manufactureRouter.get('/manufactures/requests', authorizationUser, getAllRequestByManufacture);
+manufactureRouter.post('/productitem/send',authorizationUser, checkCssx, sendListProductItem );
+
 
 export default manufactureRouter;
