ce2b046fa9161cf4d071472a8459a46970a244f4
Trang-K65ccuet <20020482@vnu.edu.vn>
2022-12-23 18:41:09 +0700

    dlpp product list

--- frontend/src/dlpp/components/products/ProductListDLPP.jsx
+++ frontend/src/dlpp/components/products/ProductListDLPP.jsx
@@ -4,20 +4,7 @@ import axios from "axios";
 
 const ProductListDLPP = () => {
 
-  //return id
-  const [profile, setProfile] = useState([]);
-  useEffect(() => {
-    getProfile();
-  }, []);
-  const getProfile = async () => {
-    const response = await axios.get("http://localhost:5000/profile", {withCredentials: true});
-    setProfile(response.data);
-  };
 
-  const id = profile.id;
-  //const item_dlpp = "http://localhost:5000/lot/item/"+ id;
-  console.log("http://localhost:5000/lot/item/" + id);
-  //return product list
   const [productDLPP, setProductDLPP] = useState([]);
 
   useEffect(() => {
@@ -25,24 +12,34 @@ const ProductListDLPP = () => {
   }, []);
 
   const getProductDLPP = async () => {
-    const response = await axios.get("http://localhost:5000/lot/item/" + id, {withCredentials: true});
+    const response = await axios.get("http://localhost:5000/lot/get/item/", {withCredentials: true});
     setProductDLPP(response.data);
   };
 
   return (
     <div>
       <h1 className="title">Sản phẩm</h1>
-      <h2 className="subtitle">Danh sách các sản phẩm và số lượng</h2>
+      <h2 className="subtitle">Các sản phẩm trong kho của đại lý</h2>
       <table className="table is-striped is-fullwidth">
         <thead>
           <tr>
             <th>STT</th>
+            <th>Mã sản phẩm</th>
             <th>Sản phẩm</th>
             <th>Dòng dản phẩm</th>
-            <th>Số lượng</th>
+            <th>Giá</th>
           </tr>
         </thead>
         <tbody>
+          {productDLPP.map((product, index) => (
+            <tr key={product.productcode}>
+            <td>{index + 1}</td>
+            <td>{product.productcode}</td>
+            <td>{product.productline}</td>
+            <td>{product.name}</td>
+            <td>{product.price}</td>
+          </tr>
+          ))}
         </tbody>
       </table>
     </div>
