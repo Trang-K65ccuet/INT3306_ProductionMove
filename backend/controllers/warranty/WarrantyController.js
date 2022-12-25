9a544a271faae5a5c9a8f455460ce5a7368a0764
phanducmanhk65 <phanducmanh2812002@gmail.com>
2022-12-25 13:08:43 +0700

    manh push

--- backend/controllers/warranty/WarrantyController.js
+++ backend/controllers/warranty/WarrantyController.js
@@ -1,8 +1,9 @@
-import { ProductItem } from "../../models/product/ProductItemModel";
-import Warranty from "../../models/warranty/WarrantyModel";
+import { database } from "../../config/Database.js";
+import { ProductItem } from "../../models/product/ProductItemModel.js";
+import Warranty from "../../models/warranty/WarrantyModel.js";
 
 // lấy ra tất cả các sản phẩm cần bảo hành theo từng trung tâm bảo hành
-export const AllItemWarranty = async (req, res) => {
+export const allItemWarranty = async (req, res) => {
     try {
     const allitem = await Warranty.findAll({
         where: {
@@ -37,5 +38,26 @@ export const sendfixedItem = async (req, res) => {
         res.status(400).json({msg: error});
     }
 }
-// gửi sản phẩm không thể sửa chữa về 
+// sản phẩm không thể sửa, cần trả lại nhà máy
+export const setCannotFIxItem = async (req, res) => {
+    const {productcode} = req.body;
+    try {
+        const sql = "SELECT * FROM productitems INNER JOIN warranties ON productitems.productcode = warranties.productcode WHERE "
+    + "productitems.productcode = :product_code AND warrantyAgentId = :id_warranty AND status = 4";
+    const item = await database.query(sql, {replacements: {
+    product_code: productcode, 
+    id_warranty: req.Id
+    }});
+    if (item.length == 0) return res.status(400).json({msg: "Không tồn tại sản phẩm có code này trong kho bảo hành"});
+    await ProductItem.update({status: 7}, {
+    where: {
+        productcode: productcode
+    }
+})
+   res.status(200).json({msg: "Chuyển sản phẩm về trạng thái không thể sửa chữa thành công"});     
+    } catch (error) {
+   res.status(400).json({msg: error});
+    }
+
+}
 
--- backend/routes/productRoute/ProductLineRoute.js
+++ backend/routes/productRoute/ProductLineRoute.js
@@ -2,9 +2,17 @@ import  express  from "express";
 import { authorizationUser, checkAdmin} from "../../middleware/AuthUser.js";
 import { getProductLine, addProductLine,updateProductLine, deleteProductLine } from "../../controllers/product/ProductLineController.js";
 const linerouter = express.Router();
+
+// lấy ra tất cả danh mục sản phẩm
 linerouter.get('/productline', authorizationUser,getProductLine );
+
+//thêm danh mục sản phẩm
 linerouter.post('/productline/add',authorizationUser,checkAdmin, addProductLine);
+
+//xóa danh mục sản phẩm
 linerouter.delete('/productline/delete',authorizationUser, checkAdmin,deleteProductLine );
+
+// sửa danh mục sản phẩm
 linerouter.put('/productline/update', authorizationUser, checkAdmin, updateProductLine);
 
 export default linerouter;
