be68ac228c79fddec317b213667d5d516749105f
phanducmanhk65 <phanducmanh2812002@gmail.com>
2022-12-27 20:53:37 +0700

    manh up

--- backend/controllers/product/ProductItemController.js
+++ backend/controllers/product/ProductItemController.js
@@ -22,7 +22,7 @@ export const productStatistic = async (req, res) => {
         const sql2 = "SELECT COUNT(*) as total, productline FROM productitems GROUP BY productline";
         const x1 = await database.query(sql1,{type: QueryTypes.SELECT});
         const x2 = await database.query(sql2,{type: QueryTypes.SELECT});
-        return res.status(200).json([x1, x2]);
+        return res.status(200).json({x1, x2});
     } catch (error) {
         return res.status(400).json({msg: error});
     }
@@ -36,7 +36,7 @@ export const spdaban = async (req, res) => {
         const sql2 = "SELECT COUNT(*) as total, productline, SUM(productitems.price) FROM transactions LEFT JOIN productitems ON transactions.productcode = productitems.productcode GROUP BY productline";
         const x1 = await database.query(sql1,{type: QueryTypes.SELECT});
         const x2 = await database.query(sql2,{type: QueryTypes.SELECT});
-        return res.status(200).json([x1, x2]);
+        return res.status(200).json({x1, x2});
     } catch (error) {
         return res.status(400).json({msg: error});
     }
@@ -48,24 +48,52 @@ export const AllFaultItem = async(req, res) => {
         const sql2 = "SELECT COUNT(*) as detailproductline, productitems.productline FROM productitems WHERE productitems.status >2 AND productitems.status < 10 GROUP BY productline";
         const total = await database.query(sql1, {type: QueryTypes.SELECT});
         const detail = await database.query(sql2, {type: QueryTypes.SELECT});
-        return res.status(200).json([total, detail]);
+        return res.status(200).json({total, detail});
     } catch (error) {
         return res.status(400).json({msg: error});
     }
 }
 // cssx
+// thống kê sản phẩm đã tạo
 export const productStatisticManufacture = async (req, res) => {
     try {
         const sql1 = "SELECT COUNT(*) as totalquantity , MONTH(productitems.dateOfManufacture) as month, YEAR(productitems.dateOfManufacture) as year FROM productitems WHERE manufactureid = :manu_id GROUP BY year, month";
-        const sql2 = "SELECT COUNT(*) as total, productline FROM productitems GROUP BY productline";
+        const sql2 = "SELECT COUNT(*) as total, productline FROM productitems WHERE manufactureid = :manu_id GROUP BY productline";
         const x1 = await database.query(sql1,{replacements: {
             manu_id: req.Id
         },type: QueryTypes.SELECT});
-        const x2 = await database.query(sql2,{type: QueryTypes.SELECT});
-        return res.status(200).json([x1, x2]);
+        const x2 = await database.query(sql2,{replacements: {
+            manu_id: req.Id
+        },type: QueryTypes.SELECT});
+        return res.status(200).json({x1, x2});
     } catch (error) {
         return res.status(400).json({msg: error});
     }
 
 }
+export const spdabanManufacture = async (req, res) => {
+    try {
+        const sql1 = "SELECT COUNT(*) as totalquantity, SUM(productitems.price) as totalmoney FROM transactions LEFT JOIN productitems ON productitems.productcode = transactions.productcode"
+        + " WHERE productitems.manufactureid = :manu_id";
+        const sql2 = "SELECT COUNT(*) as total, productline, SUM(productitems.price) as totalmoney FROM transactions LEFT JOIN productitems ON transactions.productcode = productitems.productcode WHERE productitems.manufactureid = :manu_id GROUP BY productline";
+        const x1 = await database.query(sql1,{replacements: {
+            manu_id: req.Id
+        },type: QueryTypes.SELECT});
+        const x2 = await database.query(sql2,{replacements: {
+            manu_id: req.Id
+        },type: QueryTypes.SELECT});
+        return res.status(200).json({x1, x2});
+    } catch (error) {
+        return res.status(400).json({msg: error});
+    }
+}
+// sản phẩm đã từng hoặc đang bảo hành
+export const itemNeedWarrantyManufacture = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+// sản phẩm lỗi trả về nhà sản xuất
 
--- backend/routes/productRoute/ProductItemRoute.js
+++ backend/routes/productRoute/ProductItemRoute.js
@@ -1,9 +1,10 @@
-import { getProductItem, productStatistic, spdaban, AllFaultItem} from "../../controllers/product/ProductItemController.js";
+import { getProductItem, productStatistic, spdaban, AllFaultItem, productStatisticManufacture, spdabanManufacture} from "../../controllers/product/ProductItemController.js";
 import { authorizationUser,checkAdmin,checkCssx } from "../../middleware/AuthUser.js";
 import express from 'express';
 const itemRoute = express.Router();
 // lấy tất cả các sản phẩm đã được sản xuất
 itemRoute.get('/productitem/all', authorizationUser,checkAdmin,getProductItem);
+// thống kê của admin
 // thống kê các sản phẩm đã sản xuất(tổng cộng và theo dòng sp) theo năm
 itemRoute.get('/productitem/statistic', authorizationUser,checkAdmin,productStatistic);
 //thống kê các sản phẩm đã bán ( số lượng - theo tổng và theo dòng)
@@ -11,6 +12,11 @@ itemRoute.get('/productitem/byproductline', authorizationUser,checkAdmin,spdaban
 // thống kê các sản phẩm bị lỗi
 itemRoute.get('/productitem/fault', authorizationUser, checkAdmin,AllFaultItem );
 
-//đây là các thống kê cho cơ sở sx
+//thống kê cơ sở sản xuất
+
+itemRoute.get('/productitem/statisticmanufacture', authorizationUser, checkCssx, productStatisticManufacture);
+
+//thống kê sản phẩm đã bán
+itemRoute.get('/productitem/selledmanufacture', authorizationUser, checkCssx, spdabanManufacture);
 
 export default itemRoute;
