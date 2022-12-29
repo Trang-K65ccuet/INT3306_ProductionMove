677521f7350f5d2e0e78f883b52d977be29d3ced
phanducmanhk65 <phanducmanh2812002@gmail.com>
2022-12-29 12:13:24 +0700

    manh up

--- backend/controllers/product/ProductItemController.js
+++ backend/controllers/product/ProductItemController.js
@@ -18,7 +18,7 @@ export const getProductItem = async (req, res) => {
 // thống kê số sản phẩm đã tạo theo từng dòng sản phẩm
 export const productStatistic = async (req, res) => {
     try {
-        const sql1 = "SELECT COUNT(*) as totalquantity , MONTH(productitems.dateOfManufacture) as month, YEAR(productitems.dateOfManufacture) as year FROM productitems GROUP BY year, month";
+        const sql1 = "SELECT COUNT(*) as totalquantity , MONTH(productitems.dateOfManufacture) as month, YEAR(productitems.dateOfManufacture) as year FROM productitems GROUP BY year, month ORDER BY year ASC, month ASC";
         const sql2 = "SELECT COUNT(*) as total, productline FROM productitems GROUP BY productline";
         const x1 = await database.query(sql1,{type: QueryTypes.SELECT});
         const x2 = await database.query(sql2,{type: QueryTypes.SELECT});
@@ -41,7 +41,8 @@ export const ssa = async (req, res) => {
 // thống kê số sản phẩm đã bán
 export const spdaban = async (req, res) => {
     try {
-        const sql1 = "SELECT COUNT(*) as totalquantity, SUM(productitems.price) as totalmoney FROM transactions LEFT JOIN productitems ON productitems.productcode = transactions.productcode";
+        const sql1 = "SELECT COUNT(*) as totalquantity, SUM(productitems.price) as totalmoney, YEAR(transactions.dateOfTransaction) as year,MONTH(transactions.dateOfTransaction) as month FROM transactions LEFT JOIN productitems ON productitems.productcode = transactions.productcode GROUP BY year, month"
+        + " ORDER BY year ASC, month ASC";
         const sql2 = "SELECT COUNT(*) as total, productline, SUM(productitems.price) FROM transactions LEFT JOIN productitems ON transactions.productcode = productitems.productcode GROUP BY productline";
         const x1 = await database.query(sql1,{type: QueryTypes.SELECT});
         const x2 = await database.query(sql2,{type: QueryTypes.SELECT});
@@ -53,8 +54,8 @@ export const spdaban = async (req, res) => {
 // thống kê sản phẩm bị lỗi
 export const AllFaultItem = async(req, res) => {
     try {
-        const sql1 = "SELECT COUNT(*) as total, productitems.productline FROM productitems WHERE status >2 AND status < 10";
-        const sql2 = "SELECT COUNT(*) as detailproductline, productitems.productline FROM productitems WHERE productitems.status >2 AND productitems.status < 10 GROUP BY productline";
+        const sql1 = "SELECT COUNT(*) as total, productitems.productline, YEAR(productitems.dateOfManufacture) AS year FROM productitems WHERE status >2 AND status < 10 GROUP BY year ORDER BY year ASC";
+        const sql2 = "SELECT COUNT(*) as detailproductline, productitems.productline, YEAR(productitems.dateOfManufacture) AS year FROM productitems WHERE productitems.status >2 AND productitems.status < 10 GROUP BY productline, year ORDER BY year ASC";
         const totals = await database.query(sql1, {type: QueryTypes.SELECT});
         const detail = await database.query(sql2, {type: QueryTypes.SELECT});
         return res.status(200).json([totals, detail]);
